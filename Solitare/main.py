d191e3d10d97119d583cbe7fd128ca8c4680f85e
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-02-08 13:48:55 +0330

    use init-right-frame

--- Solitare/main.py
+++ Solitare/main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 from cards import Cards
-from init_widgets import init_window , init_left_frame
+from init_widgets import init_window , init_left_frame, init_right_frame
 
 window = Tk()
 
@@ -11,6 +11,7 @@ init_window(window)
 
 init_left_frame(window, cards)
 
+init_right_frame(window, cards)
 
 
 window.mainloop()
