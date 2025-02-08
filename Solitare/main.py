3aaf9b8e88dd61c855e9658cf952a87147550be0
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-02-08 12:04:04 +0330

    top left frame finished

--- Solitare/main.py
+++ Solitare/main.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+from cards import Cards
+
 window = Tk()
 window.geometry("1000x600")  # Set window size
+
+cards = Cards()
 
 # Create canvas for background
 canvas = Canvas(window, width=1000, height=600)
@@ -19,23 +23,28 @@ canvas.image = photo  # Keep reference
 top_left = Frame(window, bg='', width=500, height=110)  # Set explicit size
 top_left.grid(row=0, column=0, columnspan=4)
 
-WIDTH = 9
-HEIGHT = 6
+WIDTH = 80
+HEIGHT = 120
+
 
 # Add padding to all_cards
-all_cards = Button(top_left, width=WIDTH, height=HEIGHT, bg='white')
+all_cards_img = ImageTk.PhotoImage(Image.open(cards.get_back()).resize((WIDTH, HEIGHT)))
+all_cards = Button(top_left, width=WIDTH, height=HEIGHT, bg='white', image=all_cards_img)
 all_cards.grid(row=0, column=0, padx=(10, 5), pady=(10, 5))
 
 # Add padding to card1
-card1 = Button(top_left, width=WIDTH, height=HEIGHT, bg='yellow')
+card1_img = ImageTk.PhotoImage(Image.open(cards.get_card()).resize((WIDTH, HEIGHT)))
+card1 = Button(top_left, width=WIDTH, height=HEIGHT, bg='yellow',image=card1_img)
 card1.grid(row=0, column=1, padx=(5, 5), pady=(10, 5))
 
 # Add padding to card2
-card2 = Button(top_left, width=WIDTH, height=HEIGHT, bg='white')
+card2_img = ImageTk.PhotoImage(Image.open(cards.get_card()).resize((WIDTH, HEIGHT)))
+card2 = Button(top_left, width=WIDTH, height=HEIGHT, bg='white',image=card2_img)
 card2.grid(row=0, column=2, padx=(5, 5), pady=(10, 5))
 
 # Add padding to card3
-card3 = Button(top_left, width=WIDTH, height=HEIGHT, bg='yellow')
+card3_img = ImageTk.PhotoImage(Image.open(cards.get_card()).resize((WIDTH, HEIGHT)))
+card3 = Button(top_left, width=WIDTH, height=HEIGHT, bg='yellow', image=card3_img)
 card3.grid(row=0, column=3, padx=(5, 5), pady=(10, 5))
 
 
